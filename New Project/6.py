d98e24d70f9fdca201ce4eb8cdc592a7bc38a2fa
garg10may <garg10may@gmail.com>
2014-12-24 20:47:21 +0530

    Created by Coding Ground Client

---
+++ New Project/6.py
@@ -0,0 +1,17 @@
+
+
+def sum(l):
+    c=0
+    for i in l:
+        c= c+i
+    print c
+    
+sum([1,2,3,4,5])
+
+def multiply(l):
+    c=1
+    for i in l:
+        c = c*i
+    print c
+    
+multiply([1,2,3,4,5])
