fe3338ae7913cad886854d4cce6f343ef022afc4
garg10may <garg10may@gmail.com>
2014-12-24 20:47:20 +0530

    Created by Coding Ground Client

---
+++ New Project/27.py
@@ -0,0 +1,17 @@
+
+
+def count(n):
+    k=[]
+    for i in n:
+        k.append(len(i))
+    print k
+        
+def count1(n):
+    print map(lambda x:len(x), n)
+    
+def count2(n):
+    print [len(i) for i in n]
+
+count(['tanmay','how are you','montecarlo'])
+count1(['tanmay','how are you','montecarlo'])
+count2(['tanmay','how are you','montecarlo'])
