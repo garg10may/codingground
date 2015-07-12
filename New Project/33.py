25ca93dfc6c2be5de9df3e37179505eb6c1634ea
garg10may <garg10may@gmail.com>
2015-07-13 04:22:08 +0530

    Update 33.py

--- New Project/33.py
+++ New Project/33.py
@@ -14,15 +14,28 @@
 
 semordnilap('33.txt')'''
 
+'''
 def is_semordnilap(filepath):
     file = open(filepath)
     words = file.read().split()
     results = []
     for word1 in words:
-        for word2 in words:
+        for word2 in words:     # using two loops here will cause error since it will also print words which are palindromes
             if word1 == word2[::-1]:
                 results.append(word1)
     return results
     
     
 print is_semordnilap('34.txt')
+'''
+
+def semordnilap(x):
+    f = open(x).read()
+    words = f.split('\n') 
+    while words:
+        a = words[0]
+        words.remove(a)
+        if a[::-1] in words:
+            print a + ' and ' + a[::-1] + ' are semordnilap'
+                
+semordnilap('33.txt')
